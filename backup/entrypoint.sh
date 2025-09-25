ab4faad34a0b96b522ece4e9161363bcfe0b2557
killer23d <killer23d@yahoo.ca>
2025-09-25 16:24:08 -0700

    Update entrypoint.sh

--- backup/entrypoint.sh
+++ backup/entrypoint.sh
@@ -1,2 +1,5 @@
-# Mark successful backup for healthcheck
+# Mark successful backup
 date > /backup/last_success
+
+# Prune old backups (older than 14 days)
+find /backup -type f -name "*.tar.gz.enc" -mtime +14 -delete
